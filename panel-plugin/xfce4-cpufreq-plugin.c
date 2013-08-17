a16a636d61e86a803536066d7fbdb3577b218743
Harald Judt <h.judt@gmx.at>
2013-08-18 01:54:59 +0200

    Fix typos in strings.

--- panel-plugin/xfce4-cpufreq-plugin.c
+++ panel-plugin/xfce4-cpufreq-plugin.c
@@ -579,7 +579,7 @@ cpufreq_construct (XfcePanelPlugin *plugin)
 
 #ifdef __linux__
 	if (cpufreq_linux_init () == FALSE)
-		xfce_dialog_show_error (NULL, NULL, _("Your system is not configured correctly to support cpu frequency scaling !"));
+		xfce_dialog_show_error (NULL, NULL, _("Your system is not configured correctly to support CPU frequency scaling!"));
 
 	gtk_widget_set_size_request (GTK_WIDGET (plugin), -1, -1);
 	cpufreq_widgets ();
@@ -589,7 +589,7 @@ cpufreq_construct (XfcePanelPlugin *plugin)
 			(GSourceFunc) cpufreq_update_cpus,
 			NULL);
 #else
-	xfce_dialog_show_error (NULL, NULL, _("Your system is not supported yet !"));
+	xfce_dialog_show_error (NULL, NULL, _("Your system is not supported yet!"));
 #endif /* __linux__ */
 
 	g_signal_connect (plugin, "free-data", G_CALLBACK (cpufreq_free),
