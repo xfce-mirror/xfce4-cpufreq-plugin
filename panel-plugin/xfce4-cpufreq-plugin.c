16d1ebdc060f0096f663256a5f20150fc0e6890b
Andre Miranda <andre42m@gmail.com>
2017-05-16 21:43:37 -0300

    Update copyright year

--- panel-plugin/xfce4-cpufreq-plugin.c
+++ panel-plugin/xfce4-cpufreq-plugin.c
@@ -644,7 +644,7 @@ cpufreq_show_about(XfcePanelPlugin *plugin,
 		 "program-name", PACKAGE_NAME,
 		 "comments", _("Show CPU frequencies and governor"),
 		 "website", PLUGIN_WEBSITE,
-		 "copyright", _("Copyright (c) 2003-2016\n"),
+		 "copyright", _("Copyright (c) 2003-2017\n"),
 		 "authors", auth,
 		 NULL);
 
