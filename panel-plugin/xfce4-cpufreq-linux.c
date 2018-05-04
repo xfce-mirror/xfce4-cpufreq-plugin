2410acb0bb6261f88fb16a0406a32fd6018a947d
Andre Miranda <andre42m@gmail.com>
2018-05-04 00:12:32 -0300

    Prefer sysfs to Intel pstate in cpufreq_linux_init (Bug #13835)

--- panel-plugin/xfce4-cpufreq-linux.c
+++ panel-plugin/xfce4-cpufreq-linux.c
@@ -437,7 +437,10 @@ cpufreq_linux_init (void)
 	if (cpuFreq->cpus == NULL)
 		return FALSE;
 
-	if (g_file_test ("/sys/devices/system/cpu/intel_pstate", G_FILE_TEST_EXISTS)) {
+	if (g_file_test ("/sys/devices/system/cpu/cpu0/cpufreq", G_FILE_TEST_EXISTS))
+		return cpufreq_cpu_read_sysfs ();
+	else if (g_file_test ("/sys/devices/system/cpu/intel_pstate", G_FILE_TEST_EXISTS))
+	{
 		gboolean ret = cpufreq_cpu_intel_pstate_read ();
 
 		/* Tools like i7z show the current real frequency using the
@@ -452,9 +455,7 @@ cpufreq_linux_init (void)
 			cpuFreq->options->show_warning = FALSE;
 		}
 		return ret;
-	} else if (g_file_test ("/sys/devices/system/cpu/cpu0/cpufreq",
-						  G_FILE_TEST_EXISTS))
-		return cpufreq_cpu_read_sysfs ();
+	}
 	else if (g_file_test ("/proc/cpufreq", G_FILE_TEST_EXISTS))
 		return cpufreq_cpu_read_procfs ();
 	else
