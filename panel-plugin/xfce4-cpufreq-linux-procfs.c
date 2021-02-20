7cf547162cc9765f7915fda08ba5151acd9d2f4d
Jan Ziak <0xe2.0x9a.0x9b@gmail.com>
2021-02-20 20:20:51 +0100

    Allocate space for fgets() on the stack instead of on the heap
    Also remove redundant g_strdup() calls.

--- panel-plugin/xfce4-cpufreq-linux-procfs.c
+++ panel-plugin/xfce4-cpufreq-linux-procfs.c
@@ -39,30 +39,25 @@ cpufreq_procfs_is_available (void)
 gboolean
 cpufreq_procfs_read_cpuinfo (void)
 {
-  CpuInfo *cpu;
+  const char *const filePath = "/proc/cpuinfo";
   FILE *file;
-  gchar *freq, *filePath, *fileContent;
-  guint i = 0;
-  gboolean add_cpu;
 
-  filePath = g_strdup ("/proc/cpuinfo");
   if (!g_file_test (filePath, G_FILE_TEST_EXISTS))
-  {
-    g_free (filePath);
     return FALSE;
-  }
 
   file = fopen (filePath, "r");
 
   if (file)
   {
-    fileContent = g_new (gchar,255);
-    while (fgets (fileContent, 255, file) != NULL)
+    gchar line[256];
+    guint i = 0;
+    while (fgets (line, sizeof(line), file) != NULL)
     {
-      if (g_ascii_strncasecmp (fileContent, "cpu MHz", 7) == 0)
+      if (g_ascii_strncasecmp (line, "cpu MHz", 7) == 0)
       {
-        cpu = NULL;
-        add_cpu = FALSE;
+        CpuInfo *cpu = NULL;
+        gboolean add_cpu = FALSE;
+        gchar *freq;
 
         if (cpuFreq->cpus && cpuFreq->cpus->len > i)
           cpu = g_ptr_array_index (cpuFreq->cpus, i);
@@ -79,7 +74,7 @@ cpufreq_procfs_read_cpuinfo (void)
           add_cpu = TRUE;
         }
 
-        freq = g_strrstr (fileContent, ":");
+        freq = g_strrstr (line, ":");
 
         if (freq == NULL)
         {
@@ -91,7 +86,7 @@ cpufreq_procfs_read_cpuinfo (void)
         sscanf (++freq, "%d.", &cpu->cur_freq);
         cpu->cur_freq *= 1000;
 
-        if (add_cpu && cpu != NULL)
+        if (add_cpu)
           g_ptr_array_add (cpuFreq->cpus, cpu);
 
         ++i;
@@ -99,10 +94,7 @@ cpufreq_procfs_read_cpuinfo (void)
     }
 
     fclose (file);
-    g_free (fileContent);
   }
-
-  g_free (filePath);
 
   return TRUE;
 }
@@ -112,9 +104,8 @@ cpufreq_procfs_read_cpuinfo (void)
 gboolean
 cpufreq_procfs_read (void)
 {
-  CpuInfo *cpu;
   FILE *file;
-  gchar *filePath, *fileContent;
+  gchar *filePath;
 
   filePath = g_strdup (PROCFS_BASE);
   if (!g_file_test (filePath, G_FILE_TEST_EXISTS))
@@ -127,12 +118,12 @@ cpufreq_procfs_read (void)
 
   if (file)
   {
-    fileContent = g_new (gchar, 255);
-    while (fgets (fileContent, 255, file) != NULL)
+    gchar line[256];
+    while (fgets (line, sizeof(line), file) != NULL)
     {
-      if (g_ascii_strncasecmp (fileContent, "CPU", 3) == 0)
+      if (g_ascii_strncasecmp (line, "CPU", 3) == 0)
       {
-        cpu = g_new0 (CpuInfo, 1);
+        CpuInfo *cpu = g_new0 (CpuInfo, 1);
         cpu->max_freq = 0;
         cpu->min_freq = 0;
         cpu->cur_governor = g_new (gchar, 20);
@@ -140,7 +131,7 @@ cpufreq_procfs_read (void)
         cpu->available_governors = NULL;
         cpu->online = TRUE;
 
-        sscanf (fileContent,
+        sscanf (line,
                 "CPU %*d %d kHz (%*d %%) - %d kHz (%*d %%) - %20s",
                 &cpu->min_freq,
                 &cpu->max_freq,
@@ -153,14 +144,13 @@ cpufreq_procfs_read (void)
     }
 
     fclose (file);
-    g_free (fileContent);
   }
 
   g_free (filePath);
 
   for (guint i = 0; i < cpuFreq->cpus->len; i++)
   {
-    cpu = g_ptr_array_index (cpuFreq->cpus, i);
+    CpuInfo *cpu = g_ptr_array_index (cpuFreq->cpus, i);
     filePath = g_strdup_printf ("/proc/sys/cpu/%d/speed", i);
 
     if (!g_file_test (filePath, G_FILE_TEST_EXISTS))
