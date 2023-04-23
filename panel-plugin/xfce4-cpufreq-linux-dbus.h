f47c5f0e3132b3f6ad2834a8c2e3f90b4e6e34b6
ziggy <z4ziggy@users.noreply.github.com>
2023-04-23 15:41:47 +0300

    [Enhancement] Implement frequency/governor changes Changes are executed via cpufreq-service - a dbus service which responds to a cpufreq-plugin dbus message & exits, so no wasted resources.
    The UI also got a face-lift to reflect new changes and allow changing
    the settings for "All CPUs".
    
    This also closes bug #3428.

---
+++ panel-plugin/xfce4-cpufreq-linux-dbus.h
@@ -0,0 +1,31 @@
+/*  xfce4-cpu-freq-plugin - panel plugin for cpu informations
+ *
+ *  Copyright (c) 2023 Elia Yehuda <z4ziggy@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef XFCE4_CPUFREQ_LINUX_DBUS_H
+#define XFCE4_CPUFREQ_LINUX_DBUS_H
+
+#include <glib.h>
+#include <stdbool.h>
+#include <string>
+
+void cpufreq_dbus_set_governor(const char* governor, int cpu, bool all);
+
+void cpufreq_dbus_set_frequency(const char* frequency, int cpu, bool all);
+
+#endif /* XFCE4_CPUFREQ_LINUX_DBUS_H */
