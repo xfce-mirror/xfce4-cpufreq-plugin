0d086405d9428bb5e7ead27770804c10d9cda2ba
Gaël Bonithon <gael@xfce.org>
2024-12-24 11:42:28 +0100

    Update copyright year

--- panel-plugin/xfce4-cpufreq-plugin.cc
+++ panel-plugin/xfce4-cpufreq-plugin.cc
@@ -1005,7 +1005,7 @@ cpufreq_show_about(XfcePanelPlugin*)
      "program-name", PACKAGE_NAME,
      "comments", _("Show CPU frequencies and governor"),
      "website", PLUGIN_WEBSITE,
-     "copyright", "Copyright \302\251 2006-2023 The Xfce development team",
+     "copyright", "Copyright \302\251 2006-2024 The Xfce development team",
      "authors", auth,
      NULL);
 
